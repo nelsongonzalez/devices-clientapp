2ef93aeeccce97ec654ab355e13f087516f1b8c8
Nel Gonzalez <nelson.gonzalez@modinter.com.ec>
2018-11-07 18:09:49 -0500

    refactor: Changed new await syntax

--- src/Devices/DeviceServices.js
+++ src/Devices/DeviceServices.js
@@ -1,40 +1,40 @@
-const allDevices = () => {
-  return fetch('http://localhost:3000/devices')
-    .then(_handleException);
+const allDevices = async () => {
+  const response = await fetch('http://localhost:3000/devices');
+  return _handleException(response);
 };
 
-const getDevice = (deviceId) => {
-  return fetch(`http://localhost:3000/devices/${deviceId}`)
-    .then(_handleException);
+const getDevice = async (deviceId) => {
+  const response = await fetch(`http://localhost:3000/devices/${deviceId}`);
+  return _handleException(response);
 };
 
-const createDevice = (device) => {
-  return fetch('http://localhost:3000/devices', {
+const createDevice = async (device) => {
+  const response = await fetch('http://localhost:3000/devices', {
     method: 'POST',
     body: JSON.stringify(device),
     headers: {
       'Content-Type': 'application/json'
     }
-  })
-    .then(_handleException);
+  });
+  return _handleException(response);
 };
 
-const updateDevice = (deviceId, device) => {
-  return fetch(`http://localhost:3000/devices/${deviceId}`, {
+const updateDevice = async (deviceId, device) => {
+  const response = await fetch(`http://localhost:3000/devices/${deviceId}`, {
     method: 'PUT',
     body: JSON.stringify(device),
     headers: {
       'Content-Type': 'application/json'
     }
-  })
-    .then(_handleException);
+  });
+  return _handleException(response);
 };
 
-const deleteDevice = (deviceId) => {
-  return fetch(`http://localhost:3000/devices/${deviceId}`, {
+const deleteDevice = async (deviceId) => {
+  const response = await fetch(`http://localhost:3000/devices/${deviceId}`, {
     method: 'DELETE'
-  })
-    .then(_handleException);
+  });
+  return _handleException(response);
 };
 
 const _handleException = (response) => {
