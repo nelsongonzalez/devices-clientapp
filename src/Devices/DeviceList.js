c2d280929135502aaedfe3b8f5909cd5417c6307
Nel Gonzalez <nelzonleonardo@gmail.com>
2018-11-08 01:41:08 -0500

    feat: Added multi-filter

--- src/Devices/DeviceList.js
+++ src/Devices/DeviceList.js
@@ -6,6 +6,7 @@ import DeviceListItem from './DeviceListItem';
 import DeleteConfirmationDialog from './DeleteConfirmationDialog';
 import Fab from "@material/react-fab";
 import MaterialIcon from "@material/react-material-icon";
+import Checkbox from '@material/react-checkbox';
 
 class DeviceList extends Component {
 
@@ -15,29 +16,53 @@ class DeviceList extends Component {
       devices: props.devices,
       filterByType: 'ALL',
       sortBy: 'SYSTEM_NAME',
+      showAll: true,
+      filterByTypes: {
+        WINDOWS_WORKSTATION: false,
+        WINDOWS_SERVER: false,
+        MAC: false
+      }
     };
     this._filterDevices = this._filterDevices.bind(this);
     this._sortDevices = this._sortDevices.bind(this);
+    this._manageState = this._manageState.bind(this);
     this.onDelete = props.onDelete;
   }
 
   render() {
     return (
       <Main>
-        <Grid>
+        <Grid align='right'>
 
           <Row>
-            <Cell columns={12}>
+            <Cell columns={8} align='middle'>
 
-              <Select label='Device type'
-                      value={this.state.filterByType}
-                      outlined
-                      onChange={(event) => this.setState({filterByType: event.target.value})}>
-                <option value='ALL'>All</option>
-                <option value='WINDOWS_WORKSTATION'>Windows Workstation</option>
-                <option value='WINDOWS_SERVER'>Windows Server</option>
-                <option value='MAC'>Mac</option>
-              </Select>
+              <FilterPanel>
+
+                <FilterPanelTitle/>
+
+                <Checkbox
+                  nativeControlId='my-checkbox'
+                  checked={this.state.filterByTypes.WINDOWS_WORKSTATION}
+                  onChange={(event, key = 'WINDOWS_WORKSTATION') => this._manageState(event, key)}/>
+                <label className='label' htmlFor='my-checkbox'>Windows Workstation</label>
+
+                <Checkbox
+                  nativeControlId='my-checkbox1'
+                  checked={this.state.filterByTypes.WINDOWS_SERVER}
+                  onChange={(event, key = 'WINDOWS_SERVER') => this._manageState(event, key)}/>
+                <label className='label' htmlFor='my-checkbox1'>Windows Server</label>
+
+                <Checkbox
+                  nativeControlId='my-checkbox2'
+                  checked={this.state.filterByTypes.MAC}
+                  onChange={(event, key = 'MAC') => this._manageState(event, key)}/>
+                <label className='label' htmlFor='my-checkbox2'>Mac</label>
+              </FilterPanel>
+
+            </Cell>
+
+            <Cell columns={4}>
 
               <Select label='Sort by' value={this.state.sortBy}
                       outlined
@@ -78,11 +103,22 @@ class DeviceList extends Component {
     );
   }
 
+  _manageState(event, key) {
+    const filterByTypes = this.state.filterByTypes;
+    filterByTypes[key] = event.target.checked;
+    const showAll = Object
+      .keys(filterByTypes)
+      .map((type) => filterByTypes[type])
+      .every((value, index, array) => value === array[0]);
+    this.setState({filterByTypes: filterByTypes, showAll: showAll});
+  }
+
   _filterDevices(device) {
-    if (this.state.filterByType === 'ALL') {
+    if (this.state.showAll) {
       return true;
     }
-    return device.type === this.state.filterByType;
+
+    return this.state.filterByTypes[device.type] === true;
   }
 
   _sortDevices(deviceA, deviceB) {
@@ -98,6 +134,18 @@ const Main = (props) => (
   <div {...props}>
 
   </div>
+);
+
+const FilterPanel = (props) => (
+  <span className='filter' {...props}>
+
+  </span>
+);
+
+const FilterPanelTitle = (props) => (
+  <span className='icon' {...props}>
+    <MaterialIcon icon='tune'/> <span className='icon-text'>DEVICE TYPE</span>
+  </span>
 );
 
 export default DeviceList;
